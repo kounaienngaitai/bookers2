a3da05316c8a05e2d4d6b61924773d195b64d259
EC2 Default User <ec2-user@ip-172-31-7-196.ap-northeast-1.compute.internal>
2024-01-03 15:18:32 +0000

    [Add]Bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+   has_many :book, dependent: :destroy
+
   has_one_attached :profile_image
 
   def get_profile_image(width, height)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
   devise_for :users
   root to: "homes#top"
   resources :users, only: [:show, :edit]
