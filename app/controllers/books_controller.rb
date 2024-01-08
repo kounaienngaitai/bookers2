20aef0c3cbf510fbd2b0498e764ad6489c50fab6
EC2 Default User <ec2-user@ip-172-31-7-196.ap-northeast-1.compute.internal>
2024-01-08 15:59:36 +0000

    [Add]各ﾍﾟｰｼﾞ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,27 +1,44 @@
 class BooksController < ApplicationController
 
   def create
-    book = Book.new(book_params)
-    book.user_id = current_user.id
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @book_new = Book.new
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
   end
 
 
   def index
+    @book_new = Book.new
     @books = Book.all
     @user = current_user
   end
 
   def show
+    @book_new = Book.new
     @book = Book.find(params[:id])
     @user = @book.user
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
-
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      redirect_to book_path(book), notice: 'You have updated user successfully.'
+    else
+      @book = Book.find(params[:id])
+      render :edit
+    end
+  end
 
 private
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,33 @@
 class UsersController < ApplicationController
 
   def index
+    @book_new = Book.new
     @user = current_user
+    @users = User.all
   end
 
   def show
+    @book_new = Book.new
     @user = User.find(params[:id])
     @books = @user.book
   end
 
   def edit
+    @user = current_user
   end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'You have updated user successfully.'
+    else
+      render :edit
+    end
+  end
+
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image,:introduction)
+  end
+
 end
